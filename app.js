c3b6cd0437f08e7c4188268f7d4c882e8b452371
Evan Prodromou <evan@fuzzy.ai>
2017-10-24 23:52:15 +0200

    Serve index file with ExpressJS

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'src')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(8080)
+
+console.log('Server listening on localhost:8080')
